3d403ab9eb2e76c9133bfb884bf240f2eb943259
Gianluca Bine <gian_bine@hotmail.com>
2018-12-07 14:51:09 -0200

    Adequação em testes

---
+++ tests/BaseTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Kolina\CustomerBundle\tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+abstract class BaseTest extends WebTestCase
+{
+    /**
+     * @return \Symfony\Bundle\FrameworkBundle\Client
+     */
+    protected function getClient()
+    {
+        return static::createClient();
+    }
+
+    /**
+     * @return null|\Symfony\Component\DependencyInjection\ContainerInterface
+     */
+    protected function getContainer()
+    {
+        return $this->getClient()->getContainer();
+    }
+
+    /**
+     * @param $id
+     * @return object
+     */
+    protected function getService($id)
+    {
+        return $this->getContainer()->get($id);
+    }
+}
